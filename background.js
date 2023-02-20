c2ca4ff394d3fe570c90f52bd6f04f95fecdb514
Piyush <piyushkhemka123@gmail.com>
2023-02-20 02:58:17 -0500

    content script that works

--- background.js
+++ background.js
@@ -1,23 +1,3 @@
 // Register the service worker
-const log = chrome.extension.getBackgroundPage().console.log;
-navigator.serviceWorker.register('background.js');
-
-log("lets see");
+console.log("Here in background.js before anything starts");
 console.log("Here in background.js - outside registry");
-
-
-// Add event listener for browserAction clicks
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({'url': "https://streeteasy.com/"}, function(tab) {
-    log("sleep now");
-    console.log("Here in background.js");
-    chrome.windows.create({
-      'tabId': tab.id,
-      'type': 'popup',
-      'width': 500,
-      'height': 600,
-      'left': 0,
-      'top': 0
-    });
-  });
-});
